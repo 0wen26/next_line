d2280b11716fc09bcf7dcf90e22cdc7244207ada
Ruben Herraiz Parache <rherraiz@car1s2.42barcelona.com>
2024-04-08 19:31:57 +0200

    b

--- nextthenext/get_next_line.c
+++ nextthenext/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rherraiz <rherraiz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 19:02:57 by rherraiz          #+#    #+#             */
-/*   Updated: 2024/04/08 18:04:34 by rherraiz         ###   ########.fr       */
+/*   Updated: 2024/04/08 18:11:31 by rherraiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,59 +27,38 @@ char	*next_line()
 }
 
 
-char	*ridbuff(int fd, char *storage)
+char	*ridbuff(int fd, char *read_buffer)
 {
-	int		rid;
-	char	*buffer;
-
-	rid = 1;
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return (ft_free(&storage));
-	buffer[0] = '\0';
-	while (rid != 0 && !(ft_strchr(buffer, '\n')))
-	{
-		rid = read(fd, buffer, BUFFER_SIZE);
-		if (rid == -1)
-		{
-			free(buffer);
-			return (NULL);
-		}
-		buffer[rid] = '\0';
-		storage = ft_strjoin(storage, buffer);
-	}
-	free(buffer);
-	if (rid == -1)
-		return (ft_free(&storage));
-	return (storage);
+	
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*storage;
-	char		*line;
-	int			i;
+	static char	*read_buffer;
+	char		*read_content;
+	int			read_bytes;
 
-	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	read_bytes = 1;
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
 		return (NULL);
-	//if (!storage)
-		//storage = ft_strdup("");
-	if ((storage && !ft_strchr(storage, '\n')) || !storage)
-		storage = ridbuff(fd, storage);
-	if (!storage)
+	read_content = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!read_content)
 		return (NULL);
-	while (storage[i] != '\n' && storage[i])
-		i++;
-	line = ft_substr(storage, 0, i + 1);
-	if (!line)
-		return (NULL);
-	storage = ft_substr(storage, i + 1, (ft_strlen(storage)) - i);
-	if (!storage)
-		return (NULL);
-	return (line);
+	while (!(ft_strchr(read_buffer, '\n') && read_bytes != 0))
+	{
+		read_bytes = read(fd, read_buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			return (NULL);
+		}
+		read_content[read_bytes] = '\0';
+		read_buffer = ft_strjoin(read_buffer, read_content);
+	}
+	
+	return(read_content);
+
 }
-/*
+
 int main(void)
 {
 	int fd = open("multiple_nl.txt", O_RDONLY);
@@ -94,4 +73,4 @@ int main(void)
 		i++;
 	}
 	return (0);
-}*/
+}
---
+++ nextthenext/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rherraiz <rherraiz@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 19:02:11 by rherraiz          #+#    #+#             */
+/*   Updated: 2024/04/08 17:53:43 by rherraiz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_free(char **str);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+
+#endif
